d22df61f270b52e28efa44aad9c30039cdc4d8fe
benitokun123 <e0313517@u.nus.edu>
2021-11-02 19:38:11 +0800

    train rl-algo against rl-algo2 after training rl-algo against python-algo

--- rl-algo2/rl_strategy.py
+++ rl-algo2/rl_strategy.py
@@ -92,11 +92,11 @@ class AlgoStrategy(gamelib.AlgoCore):
         # Initialise agents
         self.defence_agent = Agent(fname=(defence_agent_file, defence_memo_file, defence_epsilon_file), 
                                    alpha=0.005, gamma=1, num_actions=len(self.defence_actions), 
-                                   memory_size=10000, batch_size=64, epsilon_min=0.01, epsilon_dec=0.9996,
+                                   memory_size=10000, batch_size=64, epsilon_min=0.01, epsilon_dec=0.996,
                                    input_shape=425, epsilon=1)
         self.attack_agent = Agent(fname=(attack_agent_file, attack_memo_file, attack_epsilon_file), 
                                   alpha=0.005, gamma=1, num_actions=len(self.attack_actions), 
-                                  memory_size=10000, batch_size=64, epsilon_min=0.01, epsilon_dec=0.9996,
+                                  memory_size=10000, batch_size=64, epsilon_min=0.01, epsilon_dec=0.996,
                                   input_shape=425, epsilon=1)
 
 
